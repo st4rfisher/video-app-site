156b84a4f9b0477476ac7904ac4168ccf61d6441
Starfisher <supremeeworker.com>
2021-12-01 17:52:59 +0300

    feat: добавление функциональности меню

--- source/js/modules/menu-show.js
+++ source/js/modules/menu-show.js
@@ -1,18 +1,22 @@
 const menu = document.querySelector('.menu');
+const menuList = menu.querySelector('.menu__list');
 const closeButton = menu.querySelector('.menu__close');
 const openButton = document.querySelector('.header__menu-button');
 
 document.addEventListener('DOMContentLoaded', () => {
-  menu.classList.remove('menu--no-js')
-  openButton.classList.add('header__menu--show')
-  closeButton.classList.add('menu__close-button--show')
-  menu.classList.remove('menu--show')
+  menu.classList.remove('menu--no-js');
+  // menu.style.visibility='hidden';
+  openButton.classList.add('header__menu--show');
+  closeButton.classList.add('menu__close-button--show');
+  menu.classList.remove('menu--show');
 });
 
 closeButton.addEventListener('click', () => {
-  menu.classList.remove('menu--show')
+  menu.classList.remove('menu--show');
+  menuList.classList.remove('menu__list--show');
 });
 
 openButton.addEventListener('click', () => {
-  menu.classList.add('menu--show')
+  menu.classList.add('menu--show');
+  menuList.classList.add('menu__list--show');
 });
